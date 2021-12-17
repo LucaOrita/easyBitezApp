fee265750c834481b9c829c9418fd5eea97a2cba
LucaOrita <33527626+LucaOrita@users.noreply.github.com>
2021-12-17 16:16:47 +0200

    Delete src/main/resources/js directory

--- src/main/resources/js/deleteStorage.js
+++
@@ -1,6 +0,0 @@
-$(document).ready(function(){
-    window.localStorage.removeItem("m2");
-    window.localStorage.removeItem("m3");
-    window.localStorage.removeItem("p1date");
-    window.localStorage.removeItem("code");
-})
