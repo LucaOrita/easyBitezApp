ef0a49473928e6b9f808dc6b599454214526042e
LucaOrita <33527626+LucaOrita@users.noreply.github.com>
2021-12-17 16:17:15 +0200

    Add files via upload
    some js

---
+++ src/main/resources/static/js/deleteStorage.js
@@ -0,0 +1,6 @@
+$(document).ready(function(){
+    window.localStorage.removeItem("m2");
+    window.localStorage.removeItem("m3");
+    window.localStorage.removeItem("p1date");
+    window.localStorage.removeItem("code");
+})
