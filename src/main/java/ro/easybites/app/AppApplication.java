0759158dfad5552d572fa59f7bc9a66111d2ce4d
LucaOrita <33527626+LucaOrita@users.noreply.github.com>
2021-12-20 00:59:06 +0200

    https

--- src/main/java/ro/easybites/app/AppApplication.java
+++ src/main/java/ro/easybites/app/AppApplication.java
@@ -15,11 +15,11 @@ import org.springframework.context.annotation.Bean;
 public class AppApplication {
 
 //---------comment for local run
-//    @Value("${server.port.http}")
-//    private int serverPortHttp;
-//
-//    @Value("${server.port}")
-//    private int serverPortHttps;
+    @Value("${server.port.http}")
+    private int serverPortHttp;
+
+    @Value("${server.port}")
+    private int serverPortHttps;
 //-------------------------------
 
 
@@ -32,35 +32,35 @@ public class AppApplication {
 
 
 //---------comment for local run
-//    @Bean
-//    public ServletWebServerFactory servletWebServerFactory(){
-//        TomcatServletWebServerFactory tomcat = new TomcatServletWebServerFactory(){
-//
-//            @Override
-//            protected void postProcessContext(Context context){
-//                SecurityConstraint securityConstraint = new SecurityConstraint();
-//                securityConstraint.setUserConstraint("CONFIDANTIAL");
-//                SecurityCollection collection = new SecurityCollection();
-//                collection.addPattern("/*");
-//                securityConstraint.addCollection(collection);
-//                context.addConstraint(securityConstraint);
-//            }
-//
-//        };
-//
-////        tomcat.addAdditionalTomcatConnectors(httpToHttpsRedirectConnection());
-//
-//        return tomcat;
-//    }
-//
-//    private Connector httpToHttpsRedirectConnection() {
-//        Connector connector = new Connector("org.apache.coyote.http11.Http11NioProtocol");
-//        connector.setScheme("http");
-//        connector.setSecure(false);
-//        connector.setPort(serverPortHttp);
-//        connector.setRedirectPort(serverPortHttps);
-//        return connector;
-//    }
+    @Bean
+    public ServletWebServerFactory servletWebServerFactory(){
+        TomcatServletWebServerFactory tomcat = new TomcatServletWebServerFactory(){
+
+            @Override
+            protected void postProcessContext(Context context){
+                SecurityConstraint securityConstraint = new SecurityConstraint();
+                securityConstraint.setUserConstraint("CONFIDANTIAL");
+                SecurityCollection collection = new SecurityCollection();
+                collection.addPattern("/*");
+                securityConstraint.addCollection(collection);
+                context.addConstraint(securityConstraint);
+            }
+
+        };
+
+//        tomcat.addAdditionalTomcatConnectors(httpToHttpsRedirectConnection());
+
+        return tomcat;
+    }
+
+    private Connector httpToHttpsRedirectConnection() {
+        Connector connector = new Connector("org.apache.coyote.http11.Http11NioProtocol");
+        connector.setScheme("http");
+        connector.setSecure(false);
+        connector.setPort(serverPortHttp);
+        connector.setRedirectPort(serverPortHttps);
+        return connector;
+    }
 //-------------------------------
 
 }
